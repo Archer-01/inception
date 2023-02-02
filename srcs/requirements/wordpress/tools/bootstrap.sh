c1751a44d51ec07e1b4312b2bbe8488360f40bd8
Hamza Haddani <hhamza@student.1337.ma>
2023-02-02 19:43:03 +0000

    fix: WordPress & NGINX image creation
    Add wordpress volume to nginx
    Add static website
    Update some environment variable names

--- srcs/requirements/wordpress/tools/bootstrap.sh
+++ srcs/requirements/wordpress/tools/bootstrap.sh
@@ -1,26 +1,27 @@
 #!/bin/sh
 
-WORDPRESS_PATH="/var/www/wordpress/wordpress"
+if [ ! -f "/var/www/html/index.html" ]; then
+	chown -R nginx:nginx "/var/www/html"
 
-if [ ! -d "$WORDPRESS_PATH" ]; then
-	# Install wp-cli
-	curl -O "https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar"
-	chmod +x wp-cli.phar
-	mv wp-cli.phar /usr/local/bin/wp
+	mv /tmp/index.html /var/www/html/
 
 	# Install WordPress
-	wp core download --path="$WORDPRESS_PATH"
-	wp config create --path="$WORDPRESS_PATH"\
-					 --dbname="$WP_DB_NAME" \
+	wp core download --allow-root
+	wp config create --dbname="$WP_DB_NAME" \
 					 --dbuser="$WP_ADMIN" \
 					 --dbpass="$WP_ADMIN_PASSWORD" \
-					 --dbhost="$MARIADB_HOSTNAME"
-	wp core install --path="$WORDPRESS_PATH" \
-					--url="$WP_URL" \
-					--title="$WP_TITLE" \
-					--admin_user="$WP_ADMIN" \
-					--admin_password="$WP_ADMIN_PASSWORD" \
-					--admin_email="$WP_ADMIN_EMAIL"
+					 --dbhost="$MARIADB_HOSTNAME" \
+					 --allow-root
+
+	wp core install --url=$WP_URL/wordpress \
+					--title=$WP_TITLE \
+					--admin_user=$WP_ADMIN \
+					--admin_password=$WP_ADMIN_PASSWORD \
+					--admin_email=$WP_ADMIN_EMAIL \
+					--skip-email \
+					--allow-root
+
+	wp user create $WP_USER $WP_ADMIN_EMAIL --role=author --user_pass=$WP_USER_PASSWORD --allow-root
 fi
 
 php-fpm7
