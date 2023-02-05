e9e86c5707f6a36828d47ade53599169cb906431
Hamza Haddani <hhamza@student.1337.ma>
2023-02-05 12:21:19 +0000

    fix(wordpress): Connection to MariaDB
    Fix connection to MariaDB
    Install redis-cache plugin in wordpress

--- srcs/requirements/wordpress/tools/php-fpm-setup.sh
+++ srcs/requirements/wordpress/tools/php-fpm-setup.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
-if [ ! -f "/var/www/html/index.html" ]; then
-	chown -R nginx:nginx "/var/www/html"
+while ! mariadb -h$MARIADB_HOSTNAME -u$WP_ADMIN -p$WP_ADMIN_PASSWORD $WP_DB_NAME &> /dev/null 2>&1; do
+	sleep 1
+done
 
+if [ ! -f "/var/www/html/index.html" ]; then
 	mv /tmp/index.html /var/www/html/
 
 	# Install WordPress
@@ -12,7 +14,6 @@ if [ ! -f "/var/www/html/index.html" ]; then
 					 --dbpass=$WP_ADMIN_PASSWORD \
 					 --dbhost=$MARIADB_HOSTNAME \
 					 --allow-root
-
 	wp core install --url=$WP_URL/wordpress \
 					--title=$WP_TITLE \
 					--admin_user=$WP_ADMIN \
@@ -20,12 +21,22 @@ if [ ! -f "/var/www/html/index.html" ]; then
 					--admin_email=$WP_ADMIN_EMAIL \
 					--skip-email \
 					--allow-root
-
 	wp user create $WP_USER \
 				   $WP_USER_EMAIL \
 				   --role=author \
 				   --user_pass=$WP_USER_PASSWORD \
 				   --allow-root
+
+	# Install Redis plugin
+	wp plugin install "redis-cache" --activate --allow-root
+	wp config set "WP_REDIS_HOST" $REDIS_HOST --allow-root
+	wp config set "WP_REDIS_PORT" 6379 --raw --allow-root
+	wp config set "WP_REDIS_TIMEOUT" 1 --raw --allow-root
+	wp config set "WP_REDIS_READ_TIMEOUT" 1 --raw --allow-root
+	wp config set "WP_REDIS_DATABASE" 0 --raw --allow-root
+
+	chown -R nginx:nginx "/var/www/html"
 fi
 
+wp redis enable --allow-root
 php-fpm7
